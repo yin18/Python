86a8af34b7e5ab232aa0b1d45a6cd9a579dbb980
Weoi Yin Neoh <wneoh@spartaglobal.com>
2020-03-17 19:49:56 +0000

    second commit

--- D2W4.py
+++ D2W4.py
@@ -108,9 +108,12 @@
 
 
 # WHILE LOOP
-x = 1
-while x < 10:
-    print(f"it's working --> {x}")
-    if x == 4:
-        break
-    x = x + 1
+#x = 1
+# while x < 10:
+#     print(f"it's working --> {x}")
+#     if x == 4:
+#         break
+#     x = x + 1
+
+
+#
